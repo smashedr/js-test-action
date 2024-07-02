69466dededc81eff400b764f545c9e76142857f5
Shane <6071159+smashedr@users.noreply.github.com>
2024-07-02 14:28:57 -0700

    Test

--- dist/index.js
+++ dist/index.js
@@ -34016,16 +34016,17 @@ const { parse } = __nccwpck_require__(7657)
 
 ;(async () => {
     try {
-        // Parse Ref
         console.log('github.context:', github.context)
-        console.log('github.context.eventName:', github.context.eventName)
-        console.log('github.context.event_name:', github.context.event_name)
+
+        // Parse Ref
         console.log('github.context.ref:', github.context.ref)
-        console.log('github.context.payload.ref:', github.context.payload.ref)
+        console.log('github.context.eventName:', github.context.eventName)
+        console.log('prerelease:', github.context.payload.release?.prerelease)
 
         const value = github.context.ref.split('/', 2).join('/') + '/'
         console.log('value:', value)
-        const ref = github.context.ref.replace(value, '')
+        // const ref = github.context.ref.replace(value, '')
+        const ref = '0.1.1'
         console.log('ref:', ref)
 
         // Process Inputs
@@ -34033,10 +34034,12 @@ const { parse } = __nccwpck_require__(7657)
         console.log('images:', images)
         const extra = core.getInput('extra')
         console.log('extra:', extra)
-        const prefix = core.getInput('prefix')
-        console.log('prefix:', prefix)
         const seperator = core.getInput('seperator')
         console.log('seperator:', seperator)
+        const latest = core.getInput('latest')
+        console.log('latest:', latest)
+        const prefix = core.getInput('prefix')
+        console.log('prefix:', prefix)
 
         // Set Variables
         const { owner, repo } = github.context.repo
@@ -34045,8 +34048,31 @@ const { parse } = __nccwpck_require__(7657)
         const version = semver.parse(ref)
         console.log('version:', version)
 
+        // Parse Images
+        const parsedImages = parse(images, {
+            delimiter: ',',
+            trim: true,
+            relax_column_count: true,
+        }).flat()
+        console.log('parsedImages:', parsedImages)
+
         // Collect Tags
-        let extraTags = []
+        const collectedTags = []
+        if (ref) {
+            collectedTags.push(ref)
+        }
+        if (latest === 'default') {
+            if (
+                github.context.eventName === 'release' &&
+                !github.context.payload.release?.prerelease
+            ) {
+                console.log('++++ LATEST +++++')
+                collectedTags.push('latest')
+            }
+        } else if (latest === 'true') {
+            collectedTags.push('latest')
+        }
+
         if (extra) {
             const parsedTags = parse(extra, {
                 delimiter: ',',
@@ -34054,11 +34080,23 @@ const { parse } = __nccwpck_require__(7657)
                 relax_column_count: true,
             }).flat()
             console.log('parsedTags:', parsedTags)
-            if (parsedTags) {
-                extraTags = [...new Set(parsedTags)]
+            collectedTags.concat(parsedTags)
+        }
+
+        const tags = [...new Set(collectedTags)]
+        console.log('tags:', tags)
+
+        // Process Results
+        const dockerTags = []
+        for (const image of parsedImages) {
+            for (const tag of tags) {
+                dockerTags.push(`${image}:${tag}`)
             }
         }
-        console.log('extraTags:', extraTags)
+        console.log('dockerTags:', dockerTags)
+        const output = dockerTags.join(seperator)
+        console.log('output:', output)
+        core.setOutput('tags', output)
     } catch (e) {
         core.debug(e)
         core.info(e.message)
