1c0b98b8fe41b64fdd2f88ae9e6866026e6835e3
Shane <6071159+smashedr@users.noreply.github.com>
2024-07-02 13:22:54 -0700

    Test

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const semver = require('semver')
+const { parse } = require('csv-parse/sync')
 
-try {
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`)
+;(async () => {
+    try {
+        // Parse Ref
+        console.log('github.context:', github.context)
+        console.log('github.context.eventName:', github.context.eventName)
+        console.log('github.context.event_name:', github.context.event_name)
+        console.log('github.context.ref:', github.context.ref)
+        console.log('github.context.payload.ref:', github.context.payload.ref)
 
-    const chromeFile = core.getInput('chrome')
-    const firefoxFile = core.getInput('firefox')
+        const value = github.context.ref.split('/', 2).join('/') + '/'
+        console.log('value:', value)
+        const ref = github.context.ref.replace(value, '')
+        console.log('ref:', ref)
 
-    console.log(`chromeFile: ${chromeFile}!`)
-    if (chromeFile) {
-        console.log('chromeFile is TRUE')
+        // Process Inputs
+        const images = core.getInput('images')
+        console.log('images:', images)
+        const extra = core.getInput('extra')
+        console.log('extra:', extra)
+        const prefix = core.getInput('prefix')
+        console.log('prefix:', prefix)
+        const seperator = core.getInput('seperator')
+        console.log('seperator:', seperator)
+
+        // Set Variables
+        const { owner, repo } = github.context.repo
+        console.log('owner:', owner)
+        console.log('repo:', repo)
+        const version = semver.parse(ref)
+        console.log('version:', version)
+
+        // Collect Tags
+        let extraTags = []
+        if (extra) {
+            const parsedTags = parse(extra, {
+                delimiter: ',',
+                trim: true,
+                relax_column_count: true,
+            }).flat()
+            console.log('parsedTags:', parsedTags)
+            if (parsedTags) {
+                extraTags = [...new Set(parsedTags)]
+            }
+        }
+        console.log('extraTags:', extraTags)
+    } catch (e) {
+        core.debug(e)
+        core.info(e.message)
+        core.setFailed(e.message)
     }
-    console.log(`firefoxFile: ${firefoxFile}!`)
-    if (firefoxFile) {
-        console.log('firefoxFile is TRUE')
-    }
-
-    // core.setOutput("time", time);
-} catch (error) {
-    core.setFailed(error.message)
-}
+})()
