ae0112d12e69d04a046e937c5b33430b03f64aad
Shane <6071159+smashedr@users.noreply.github.com>
2024-07-22 16:03:35 -0700

    Test Local

--- src/index.js
+++ src/index.js
@@ -6,20 +6,16 @@ const github = require('@actions/github')
         // console.log('github.context:', github.context)
         // console.log('process.env:', process.env)
 
-        const token = core.getInput('token', { required: true })
-        console.log('token:', token)
         const body = core.getInput('body', { required: true })
         console.log('body:', body)
 
-        // Set Variables
+        // Example GitHub Context
         const { owner, repo } = github.context.repo
         console.log('owner:', owner)
         console.log('repo:', repo)
 
-        // console.log('github.context.ref:', github.context.ref)
-        // console.log('process.env.TEST_ENV:', process.env.TEST_ENV)
-
-        core.setOutput('result', body)
+        const result = body + ' - updated by js-test-action'
+        core.setOutput('result', result)
 
         core.info(`\u001b[32;1mFinished Success`)
     } catch (e) {
