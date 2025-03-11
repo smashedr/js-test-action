78ae461f6b18bf40da5eb581000caca88ae7c265
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2025-03-10 22:57:48 -0700

    Update Index (#10)
    * Update Index
    
    * Update Index
    
    * Update Index
    
    * Update Index

--- src/index.js
+++ src/index.js
@@ -8,48 +8,49 @@ const Tags = require('./tags')
         core.info(`🏳️ Starting JS Test Action`)
 
         // Debug
-        // console.log('github.context:', github.context)
-        // console.log('process.env:', process.env)
+        core.startGroup('Debug: github.context')
+        console.log(github.context)
+        core.endGroup() // Debug github.context
+        core.startGroup('Debug: process.env')
+        console.log(process.env)
+        core.endGroup() // Debug process.env
 
-        // Inputs
-        core.startGroup('Inputs')
-        const tag = core.getInput('tag', { required: true })
-        core.info(`tag: "${tag}"`)
-        const summary = core.getBooleanInput('summary', { required: true })
-        core.info(`summary: "${summary}"`)
-        const token = core.getInput('token', { required: true })
-        core.info(`token: "${token}"`)
+        // Process Inputs
+        const inputs = getInputs()
+        core.startGroup('Parsed Inputs')
+        console.log(inputs)
+        core.endGroup() // Inputs
 
         // Context
         const { owner, repo } = github.context.repo
-        core.info(`owner: "${owner}"`)
-        core.info(`repo: "${repo}"`)
-        core.endGroup() // Inputs
+        core.info(`owner: ${owner}`)
+        core.info(`repo: ${repo}`)
 
         const sha = github.context.sha
-        core.info(`Target sha: \u001b[32m${sha}`)
+        core.info(`Target sha: \u001b[33;1m${sha}`)
 
-        const tags = new Tags(token, owner, repo)
+        const tags = new Tags(inputs.token, owner, repo)
 
         // Action
-        // core.info(`⌛ Processing tag: "${tag}"`)
-        core.startGroup(`Processing tag: "${tag}"`)
+        core.startGroup(`Processing tag: "${inputs.tag}"`)
         let result
-        const reference = await tags.getRef(tag)
+        const reference = await tags.getRef(inputs.tag)
         // console.log('reference.data:', reference?.data)
         if (reference) {
             core.info(`current sha: ${reference.data.object.sha}`)
             if (sha !== reference.data.object.sha) {
-                core.info(`\u001b[35mUpdating tag "${tag}" to: ${sha}`)
-                await tags.updateRef(tag, sha, true)
+                core.info(`\u001b[35mUpdating tag "${inputs.tag}" to: ${sha}`)
+                await tags.updateRef(inputs.tag, sha, true)
                 result = 'Updated'
             } else {
-                core.info(`\u001b[36mTag "${tag}" already points to: ${sha}`)
+                core.info(
+                    `\u001b[36mTag "${inputs.tag}" already points to: ${sha}`
+                )
                 result = 'Not Changed'
             }
         } else {
-            core.info(`\u001b[33mCreating new tag "${tag}" to: ${sha}`)
-            await tags.createRef(tag, sha)
+            core.info(`\u001b[33mCreating new tag "${inputs.tag}" to: ${sha}`)
+            await tags.createRef(inputs.tag, sha)
             result = 'Created'
         }
         core.endGroup() // Processing
@@ -59,31 +60,9 @@ const Tags = require('./tags')
         core.setOutput('sha', sha)
 
         // Summary
-        if (summary) {
+        if (inputs.summary) {
             core.info('📝 Writing Job Summary')
-
-            core.summary.addRaw('## JS Test Action\n')
-            core.summary.addRaw(
-                `${result}: [${tag}](https://github.com/${owner}/${repo}/releases/tag/${tag}) :arrow_right: \`${sha}\`\n`
-            )
-
-            core.summary.addRaw('<details><summary>Inputs</summary>')
-            core.summary.addTable([
-                [
-                    { data: 'Input', header: true },
-                    { data: 'Value', header: true },
-                ],
-                [{ data: 'tag' }, { data: `<code>${tag}</code>` }],
-                [{ data: 'summary' }, { data: `<code>${summary}</code>` }],
-            ])
-            core.summary.addRaw('</details>\n')
-
-            const text = 'View Documentation, Report Issues or Request Features'
-            const link = 'https://github.com/smashedr/js-test-action'
-            core.summary.addRaw(
-                `\n[${text}](${link}?tab=readme-ov-file#readme)\n\n---`
-            )
-            await core.summary.write()
+            await addSummary(inputs, result, sha)
         }
 
         core.info(`✅ \u001b[32;1mFinished Success`)
@@ -93,3 +72,43 @@ const Tags = require('./tags')
         core.setFailed(e.message)
     }
 })()
+
+/**
+ * Get Inputs
+ * @return {{tag: string, summary: boolean, token: string}}
+ */
+function getInputs() {
+    return {
+        tag: core.getInput('tag', { required: true }),
+        summary: core.getBooleanInput('summary'),
+        token: core.getInput('token', { required: true }),
+    }
+}
+
+/**
+ * Add Summary
+ * @param {Object} inputs
+ * @param {String} result
+ * @param {String} sha
+ * @return {Promise<void>}
+ */
+async function addSummary(inputs, result, sha) {
+    core.summary.addRaw('## JS Test Action\n')
+    const url = `https://github.com/${github.context.payload.repository.full_name}/releases/tag/${inputs.tag}`
+    core.summary.addRaw(
+        `${result}: [${inputs.tag}](${url}) :arrow_right: \`${sha}\`\n`
+    )
+
+    delete inputs.token
+    const yaml = Object.entries(inputs)
+        .map(([k, v]) => `${k}: ${JSON.stringify(v)}`)
+        .join('\n')
+    core.summary.addRaw('<details><summary>Inputs</summary>')
+    core.summary.addCodeBlock(yaml, 'yaml')
+    core.summary.addRaw('</details>\n')
+
+    const text = 'View Documentation, Report Issues or Request Features'
+    const link = 'https://github.com/smashedr/js-test-action'
+    core.summary.addRaw(`\n[${text}](${link}?tab=readme-ov-file#readme)\n\n---`)
+    await core.summary.write()
+}
