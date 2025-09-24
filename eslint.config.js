74645e3acbc3a17dac5030885451f96725957ed9
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2025-09-23 19:55:14 -0700

    Update Build (#16)

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from '@eslint/js'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+
+export default [
+    js.configs.recommended,
+    eslintPluginPrettierRecommended,
+    {
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+        },
+        settings: {
+            env: {
+                browser: true,
+                es2021: true,
+                jquery: true,
+                webextensions: true,
+            },
+        },
+        rules: {
+            'no-undef': 'off',
+            'no-extra-semi': 'off',
+        },
+    },
+]
